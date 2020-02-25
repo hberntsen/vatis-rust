85e46e45c06fc3658943861ff35afb6db2c5442f
Bastiaan Schaap <bastiaan.schaap@siteminds.nl>
2020-02-25 10:37:03 +0100

    Implemented getting stats + erorr handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use linux_stats;
 
 fn main()
 {
-    println!("Hello World!");
+    let stat = linux_stats::stat();
+
+    let stat = match stat
+    {
+        Ok(s) => s,
+        Err(error) => {
+            panic!("error while getting stats: {:?}", error)
+        },
+    };
+
+    let mem_info = linux_stats::meminfo();
+
+    let mem_info = match mem_info {
+        Ok(m) => m,
+        Err(error) => {
+            panic!("error while getting memory stats: {:?}", error)
+        }
+    };
+
 }
